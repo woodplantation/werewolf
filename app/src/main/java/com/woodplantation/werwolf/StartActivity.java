24b6d03daada5b845d53b775c155fd395e56f5ff
DorianW <dorianw@zedat.fu-berlin.de>
2016-11-02 11:03:49 +0100

    Ich habe angefangen eine Art Wiki zu erstellen und verschiedene Optionen dafür ausgetestet #228 comment:Activity erstellt und layout getestet

--- app/src/main/java/com/woodplantation/werwolf/StartActivity.java
+++ app/src/main/java/com/woodplantation/werwolf/StartActivity.java
@@ -1,5 +1,6 @@
 package com.woodplantation.werwolf;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
@@ -31,5 +32,6 @@ public class StartActivity extends AppCompatActivity {
                 limitPlayerLayout.setVisibility(b ? View.VISIBLE : View.INVISIBLE);
             }
         });
+
     }
 }
