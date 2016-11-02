7b1de7a2f5aed029b3fd4b6eede281b33ca4f515
Sebastian Oltmanns <sebastian.oltmanns.developer@gmail.com>
2016-11-02 13:10:51 +0100

    id:218 state:done comment: Lobby wird durch Wifip2p erstellt und Adresse mit Passwort angezeigt. Die grafische Oberfläche der Startactivity muss geändert werden, da beim Erstellen keine Parameter angegeben werden können.

--- app/src/main/java/com/woodplantation/werwolf/activities/StartActivity.java
+++ app/src/main/java/com/woodplantation/werwolf/activities/StartActivity.java
@@ -1,5 +1,6 @@
-package com.woodplantation.werwolf;
+package com.woodplantation.werwolf.activities;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
@@ -9,6 +10,8 @@ import android.widget.CompoundButton;
 import android.widget.LinearLayout;
 import android.widget.Switch;
 
+import com.woodplantation.werwolf.R;
+import com.woodplantation.werwolf.graphics.MyButton;
 import com.woodplantation.werwolf.graphics.MySwitch;
 
 public class StartActivity extends AppCompatActivity {
@@ -29,6 +32,15 @@ public class StartActivity extends AppCompatActivity {
             @Override
             public void onCheckedChanged(CompoundButton compoundButton, boolean b) {
                 limitPlayerLayout.setVisibility(b ? View.VISIBLE : View.INVISIBLE);
+            }
+        });
+
+        MyButton createButton = (MyButton) findViewById(R.id.button_create_lobby);
+        createButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(StartActivity.this, LobbyActivity.class);
+                startActivity(intent);
             }
         });
     }
